37882efd79874eb003a4a57727b042d4070d29a2
Ceyda Sahin <ceydasahin889@gmail.com>
2024-05-14 02:27:13 +0300

    Update UserController.java
    POST createUser: This request creates a new user, returning a unique user ID, level,
    coins, and random country.
    PUT updateLevelAndCoins: Updates the user's level and coins. Returns updated progress data.

--- src/main/java/com/dreamgames/backendengineeringcasestudy/controller/UserController.java
+++ src/main/java/com/dreamgames/backendengineeringcasestudy/controller/UserController.java
@@ -1,5 +1,43 @@
 package com.dreamgames.backendengineeringcasestudy.controller;
 
+import com.dreamgames.backendengineeringcasestudy.dto.UserDto;
+import com.dreamgames.backendengineeringcasestudy.dto.UserUpdateLevelRequestDto;
+import com.dreamgames.backendengineeringcasestudy.entity.User;
+import com.dreamgames.backendengineeringcasestudy.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
-    //TODO
+
+    private final UserService userService;
+    @GetMapping("/user-progress/users/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id){
+        UserDto userDto = userService.findById(id);
+        return ResponseEntity.ok(userDto);
+    }
+
+    //TODO: Pagination
+    @GetMapping("/user-progress/users")
+    public ResponseEntity<List<UserDto>> getAllUsers(){
+        List<UserDto> userDtoList = userService.findAll();
+        return ResponseEntity.ok(userDtoList);
+    }
+
+    @PostMapping("/user-progress/users/register")
+    public ResponseEntity<UserDto> createUser(){
+        UserDto userDto = userService.createAndSave();
+        return ResponseEntity.ok(userDto);
+    }
+
+    @PutMapping("/user-progress/users/")
+    public ResponseEntity<UserDto> updateUserLevelAndCoins(@RequestBody UserUpdateLevelRequestDto updateLevelRequestDto){
+        UserDto userDto = userService.updateLevel(updateLevelRequestDto);
+        return ResponseEntity.ok(userDto);
+    }
+
 }
